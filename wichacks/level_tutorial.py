5c82a2ce9a4337bb1a7c99bdee11200272eca2e6
ams4222 <ams4222@g.rit.edu>
2022-02-26 10:56:11 -0500

    created file level tutorial

--- wichacks/level_tutorial.py
+++ wichacks/level_tutorial.py
@@ -2,5 +2,6 @@ import pygame, sys
 from pygame.locals import *
 
 level_tutorial_map = [
-'                                                           '
+'                                                           ',
+
 ]
